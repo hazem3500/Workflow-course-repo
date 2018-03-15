85122bf7c5735956961b2a3462047daec4102f2b
hazem osama ibrahim <hazem3500@gmail.com>
2018-03-15 23:57:39 +0200

    completed automatic sprite generation

---
+++ gulp/tasks/sprites.js
@@ -0,0 +1,53 @@
+const gulp = require('gulp');
+const svgSprite = require('gulp-svg-sprite');
+const rename = require('gulp-rename');
+const del = require('del');
+
+const config = {
+    mode: {
+        css: {
+            sprite: 'sprite.svg',
+            render: {
+                scss: {
+                    template: './gulp/templates/sprites.scss'
+                }
+            }
+        }
+    }
+};
+
+gulp.task('beginClean', () =>
+    del(['./app/temp/sprite/', './app/assets/images/sprites/'])
+);
+
+gulp.task('createSprite', () => {
+    gulp
+        .src('./app/assets/images/icons/**/*.svg')
+        .pipe(svgSprite(config))
+        .pipe(gulp.dest('./app/temp/sprite/'));
+});
+
+gulp.task('copySpriteGraphic', ['createSprite'], () => {
+    gulp
+        .src('./app/temp/sprite/css/**/*.svg')
+        .pipe(gulp.dest('./app/assets/images/sprites/'));
+});
+
+gulp.task('copySpriteSCSS', ['createSprite'], () => {
+    gulp
+        .src('./app/temp/sprite/css/*.scss')
+        .pipe(rename('_sprite.scss'))
+        .pipe(gulp.dest('./app/assets/styles/scss/modules'));
+});
+
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteSCSS'], () =>
+    del('./app/temp/sprite/')
+);
+
+gulp.task('icons', [
+    'beginClean',
+    'createSprite',
+    'copySpriteGraphic',
+    'copySpriteSCSS',
+    'endClean'
+]);
